5178901e2a67bcac696d81a046394670665a980d
giantaw1 <adrian.warner@gmail.com>
2024-08-14 12:57:05 -0400

    Add human choice function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Rock Paper Scissors game
+// Rock Paper Scissors game //
 
 // get computer choice
 
@@ -6,12 +6,24 @@ function getComputerChoice () {
     // random number between 1 and 3
     const rand = Math.floor(Math.random() * (4 - 1) + 1);
     if (rand > 2) {
-        /*return*/console.log("rock");
+        console.log("Computer chooses: rock");
+        return "rock";
     } else if (rand === 2) {
-        /*return*/console.log("paper");
+        console.log("Computer chooses: paper");
+        return "paper";
     } else {
-        /*return*/console.log("scissors");
+        console.log("Computer chooses: scissors");
+        return "scissors";
     }
 }
 
-getComputerChoice();
+
+// get human choice from prompt
+
+function getHumanChoice () {
+    const humanChoice = prompt("Please choose: Rock, Paper or Scissors");
+    console.log(`You Chose: ${humanChoice}`);
+    return humanChoice;
+}
+
+getHumanChoice();
