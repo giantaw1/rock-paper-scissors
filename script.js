c6ae0a1b8a24015f6cfab37f5f5426d89a299452
giantaw1 <adrian.warner@gmail.com>
2024-08-14 12:45:40 -0400

    Add computer choice generator function

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Rock Paper Scissors game
-console.log('hello world');
+
+// get computer choice
+
+function getComputerChoice () {
+    // random number between 1 and 3
+    const rand = Math.floor(Math.random() * (4 - 1) + 1);
+    if (rand > 2) {
+        /*return*/console.log("rock");
+    } else if (rand === 2) {
+        /*return*/console.log("paper");
+    } else {
+        /*return*/console.log("scissors");
+    }
+}
+
+getComputerChoice();
