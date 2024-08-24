02d76a3e25dcf032d5311c79451bb675d2301b2f
giantaw1 <adrian.warner@gmail.com>
2024-08-24 17:46:05 -0400

    Add functional core DOM element display. Add addtional HTML structure and styling

--- script.js
+++ script.js
@@ -14,55 +14,66 @@ function getComputerChoice () {
 }
 
 // play single round //
-// GAME RULES: rock beats scissors, scissors beats paper, paper beats rock
+// CORE GAME LOGIC
 
  // initalize score and round variables
- let humanScore = 0;
- let computerScore = 0;
- let round = 1;
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+let roundResultsText = '';
+let roundWinnerText = '';
+let scoreResultsText = '';
 
 function playRound(humanChoice, computerChoice) {
     const hc = humanChoice;
     const cc = computerChoice;
-    const roundWinner = '';
+    
     if (hc === "rock" && cc === "scissors") { // human wins
-        roundWinner = "Computer chooses Scissors - You win!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses Scissors - You win!";
         humanScore++;
-        round++;
+       
     } else if (hc === "scissors" && cc === "paper") {
-        roundWinner = "Computer chooses Paper - You win!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses Paper - You win!";
         humanScore++;
-        round++;
+        
     } else if (hc === "paper" && cc === "rock") {
-        roundWinner = "Computer chooses Rock - You win!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses Rock - You win!";
         humanScore++; 
-        round++;
+        
     } else if (hc === "scissors" && cc === "rock") { //computer wins
-        roundWinner = "Computer chooses Rock - Computer wins!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses Rock - Computer wins!";
         computerScore++;
-        round++;
+       
     } else if (hc === "paper" && cc === "scissors") {
-        roundWinner = "Computer chooses Scissors - Computer wins!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses Scissors - Computer wins!";
         computerScore++;
-        round++;
+        
     } else if (hc === "rock" && cc === "paper") {
-        roundWinner = "Computer chooses Paper - Computer wins!";
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "Computer chooses paper - Computer wins!";
         computerScore++;
-        round++;
+        
     } else if (hc === "rock" && cc === "rock") { // its a tie
-        roundWinner = "It's a tie - choose again!";
-        round++;
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "It's a tie - choose again!";
+        
     } else if (hc === "paper" && cc === "paper") {
-        roundWinner = "It's a tie - choose again!";
-        round++;
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "It's a tie - choose again!";
+        
     } else if (hc === "scissors" && cc === "scissors") {
-        roundWinner = "It's a tie - choose again!";
-        round++;
+        roundResultsText = `Player Chose: ${hc} || Computer Chose: ${cc}`;
+        roundWinnerText = "It's a tie - choose again!";
+       
     }
-    // return console.log(`Human Chose: ${hc} || Computer Chose: ${cc} \n
-    // The Score is - Computer: ${computerScore} Human: ${humanScore}`);
 }
-
 
 // RPS-UI update - grab variables from buttons
 
@@ -70,24 +81,51 @@ const rockBtn = document.getElementById('rock');
 const paperBtn = document.getElementById('paper');
 const scissorsBtn = document.getElementById('scissors');
 
-const currentRound = document.getElementById('current-score');
+// display current round 
+const currentRound = document.getElementById('current-round');
+const currentResult = document.createElement('p'); 
+currentRound.appendChild(currentResult);
+const currentResultText = document.createTextNode(roundResultsText);
+currentResult.appendChild(currentResultText);
+
+// display current round winner
+const currentWinnerDiv = document.getElementById('current-winner');
+const roundWinner = document.createElement('p');
+currentWinnerDiv.appendChild(roundWinner);
+const currentWinnerText = document.createTextNode(roundWinnerText);
+roundWinner.appendChild(currentWinnerText);
+
+// display current round and score
+const currentScore = document.getElementById('current-score');
+const scores = document.createElement('p');
+currentScore.appendChild(scores);
+const currentScoreText = document.createTextNode(scoreResultsText);
+scores.appendChild(currentScoreText);
+
+const finalResults = document.getElementById('final-results');
+
 
 // play round with player choice from button clicks
 
 function playGameBtn(playerChoice) {
     const humanSelection = playerChoice;
-    const computerSelection = getComputerChoice()
+    const computerSelection = getComputerChoice();
+
     playRound(humanSelection, computerSelection);
-    if (round === 1) {
-        const result = document.createElement('ul');
-        const currentResult = document.createElement('li');
-        currentRound.appendChild(result);
-        result.appendChild(currentResult);
-        currentResult.createTextNode('roundWinner');
-    } else if (round > 1) {
-        currentResult.createTextNode('roundWinner');
-    }
+    scoreResultsText = `Round ${round} Score is - Computer: ${computerScore} Player: ${humanScore}`;
+
+    currentRound.style.display = 'block';
+    currentScore.style.display = 'block';
+    currentWinnerDiv.style.display = 'block'; 
+
+    currentResult.innerText = roundResultsText;
+    roundWinner.innerText = roundWinnerText;
+    scores.innerText = scoreResultsText;
+    
+    round++;
+
 }
+
 
 rockBtn.addEventListener('click', e => {
     playGameBtn('rock');
@@ -108,7 +146,7 @@ scissorsBtn.addEventListener('click', e => {
 // function playGame () {
 //     for (let i = 0; i < 5; i++) {
 //         const computerSelection = getComputerChoice();
-//         const humanSelection = getHumanChoice();
+//         const humanSelection = getPlayerChoice();
 //         console.log(`Round: ${i + 1}`);
 //         playRound(humanSelection, computerSelection);
 //     }
